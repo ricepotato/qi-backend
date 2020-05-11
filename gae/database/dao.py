4142c5bfc20517d5a1e655cb3ac71f131d433372
Sukjun Sagong <ricepotato40@gmail.com>
2020-05-11 20:18:24 +0900

    initial commit

---
+++ gae/database/dao.py
@@ -0,0 +1,30 @@
+from database.mongo import get_client
+
+
+class MongoDao(object):
+    def __init__(self):
+        self.client = get_client()
+        self.coll = self.client.qi.stock
+
+    def insert(self, obj):
+        return self.coll.insert_one(obj)
+
+    def find_by_code(self, code):
+        obj = self.coll.find_one({"code": code})
+        return obj
+
+    def get_stock_list(self, market):
+        rs = self.coll.find({"market": market})
+        return list(rs)
+
+    def set_object(self, code, object):
+        return self.coll.update_one({"code": code}, {"$set": object})
+
+    def exist(self, code, obj_notation):
+        res = self.coll.find_one(
+            {"$and": [{"code": code}, {obj_notation: {"$exists": True}}]}
+        )
+        if res:
+            return True
+        else:
+            return False
