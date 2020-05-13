52714309296b3ff6122643f8fca0743dd92ff7c7
Sukjun Sagong <ricepotato40@gmail.com>
2020-05-13 21:05:00 +0900

    and stock query

--- gae/database/dao.py
+++ gae/database/dao.py
@@ -13,8 +13,15 @@ class MongoDao(object):
         obj = self.coll.find_one({"code": code})
         return obj
 
-    def get_stock_list(self, market):
-        rs = self.coll.find({"market": market})
+    def get_stock_list(self, market, mc_min=None, mc_max=None, limit=20):
+        query_and = [{"market": market}]
+        if mc_min is not None:
+            query_and.append({"market_cap": {"$gte": mc_min}})
+
+        if mc_max is not None:
+            query_and.append({"market_cap": {"$lte": mc_max}})
+
+        rs = self.coll.find({"$and": query_and}, {"_id": False}).limit(limit)
         return list(rs)
 
     def set_object(self, code, object):
